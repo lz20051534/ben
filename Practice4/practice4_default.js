10315365d9fe0d175b32cbc7220f128a18cdd09c
lz20051534 <32856844+lz20051534@users.noreply.github.com>
2018-08-14 15:53:09 +0800

    aaa
    aaa

--- Practice4/practice4_default.js
+++ Practice4/practice4_default.js
@@ -1,6 +1,8 @@
 document.getElementById('P4_buttonCaidan').onclick = function(){
     document.getElementById('P4_Tan').style.display = 'block';
+    document.body.style.overflow = 'hidden';
 };
 document.getElementById('P4_Tan_caidanClose').onclick = function(){
     document.getElementById('P4_Tan').style.display = 'none';
+    document.body.style.overflowY = 'visible';
 };
